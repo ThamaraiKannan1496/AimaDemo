8c25d4e9438eda7705c26334853d4fd264417755
ThamaraiKannan1496 <kannanbala1496@gmail.com>
2025-11-04 20:47:24 +0530

    minor

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainLayout from "../layouts/mainLayout";
+import MainLayout from "../layouts/MainLayout";
 import Dashboard from "../pages/Dashboard/Dashboard.jsx";
 
 const AppRoutes = () => {
